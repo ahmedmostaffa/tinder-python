8aa43acf86d8c18adb738f2b5d0b1b2ec4a6decd
Ahmed Mabrook <ahmed.mabrook@mondiadigital.com>
2023-07-12 10:11:29 +0300

    Adding nope method

--- Actions.py
+++ Actions.py
@@ -1,4 +1,5 @@
 from seleniumwire import webdriver
+from time import sleep
 class Actions:
     def __init__(self,driver:webdriver.Chrome):
         self.driver=driver
@@ -6,6 +7,39 @@ class Actions:
     def like(self):
         like_button = self.driver.find_element(by='xpath', value='//button//span[text()="Like"]')
         self.click(like_button);
+
+    def nope(self):
+        nope_button = self.driver.find_element(by='xpath', value='//button//span[text()="Nope"]')
+        self.click(nope_button);
+    
+
+
+    
+    def login(self, username,password):
+        
+        login_btn=self.driver.find_element(by='xpath',value='//*[@id="u490315748"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
+        login_btn.click()
+
+        # switch to login popup
+        base_window = self.driver.switch_to.window('')
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys(username)
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        pw_in.send_keys(password)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        self.driver.switch_to_window(base_window)
+
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
 
 
     def click(self,elem):
--- main_script.py
+++ main_script.py
@@ -4,18 +4,17 @@ from Actions import Actions
 
 driver = webdriver.Chrome()
  
-driver.get('https://tinder.com/app/recs')
-time.sleep(100)
+driver.get('https://tinder.com/app/matches')
+# time.sleep(100)
 driver.implicitly_wait(30)
 
 action=Actions(driver);
-action.like()
+action.login("dadaffafa","afafafaf")
  
 # Access requests list via the `requests` attribute
 for request in driver.requests:
     if request.response:
         print(
-            
             request.response.status_code,
             request.response.headers['Content-Type']
         )
