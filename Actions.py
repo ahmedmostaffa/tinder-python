c0f3c1a54fdc48ccf948eebd88bb018c9e473ed3
Ahmed Mabrook <ahmed.mabrook@mondiadigital.com>
2023-07-12 19:17:15 +0300

    ..

--- Actions.py
+++ Actions.py
@@ -1,27 +1,34 @@
 from seleniumwire import webdriver
 from time import sleep
 class Actions:
+    time=300
     def __init__(self,driver:webdriver.Chrome):
         self.driver=driver
 
     def like(self):
+        self.wait()
         like_button = self.driver.find_element(by='xpath', value='//button//span[text()="Like"]')
         self.click(like_button);
 
     def nope(self):
+        self.wait()
         nope_button = self.driver.find_element(by='xpath', value='//button//span[text()="Nope"]')
         self.click(nope_button);
     
+    def printEndpoint(self):
+        self.driver.wait_for_request('recs/core')
+        
+    
 
 
     
-    def login(self, username,password):
+    def login(self, phoneNumber:str):
+        self.driver.find_element(by='xpath',value='//div[text()="Log in"]').click()
+        # self.driver.find_element(by='xpath',value='//div[contains(text(),"Facebook")]').click()
+        # self.driver.find_element(by='css_selector',value='input[name="phone_number"]').click()
         
-        login_btn=self.driver.find_element(by='xpath',value='//*[@id="u490315748"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
-        login_btn.click()
-
+        '''
         # switch to login popup
-        base_window = self.driver.switch_to.window('')
         self.driver.switch_to_window(self.driver.window_handles[1])
 
         email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
@@ -30,8 +37,7 @@ class Actions:
         pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
         pw_in.send_keys(password)
 
-        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
-        login_btn.click()
+        self.driver.find_element(by='xpath',value='//input[@name="login"]').click()
 
         self.driver.switch_to_window(base_window)
 
@@ -40,7 +46,11 @@ class Actions:
 
         popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
         popup_2.click()
-
+        '''
+    def wait(self):
+        while((self.driver.current_url!='https://tinder.com/app/recs')):
+            sleep(5)
+        
 
     def click(self,elem):
         self.driver.execute_script("arguments[0].click();",elem) 
--- main_script.py
+++ main_script.py
@@ -1,15 +1,23 @@
 from seleniumwire import webdriver  # Import seleniumwire
-import time
+from time import sleep
 from Actions import Actions
 
+options = webdriver.ChromeOptions()
+options.add_argument("--allow-geolocation") 
+options.add_argument('--disable-infobars')
+options.add_argument('--disable-notifications')
+
 driver = webdriver.Chrome()
- 
+
 driver.get('https://tinder.com/app/matches')
 # time.sleep(100)
 driver.implicitly_wait(30)
 
 action=Actions(driver);
-action.login("dadaffafa","afafafaf")
+action.login("d")
+
+action.like()
+action.nope()
  
 # Access requests list via the `requests` attribute
 for request in driver.requests:
